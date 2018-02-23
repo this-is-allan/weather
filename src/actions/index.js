b0cccd579e6693766ff81dc6c9acaa863b8eedb5
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-02-23 13:08:01 -0300

    Building a List Container

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,8 @@ export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city}`;
     const request = axios.get(url);
     
+    console.log('Request:', request);
+    
     return {
         type: FETCH_WEATHER,
         payload: request
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
