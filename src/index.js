0962a8f715e5bdc5b3079fb79d1c0efa90b7f3f1
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-02-23 14:57:55 -0300

    google maps integration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import './index.css';
 
 import App from './components/App';
 import reducers from './reducers';
